11abc7549638f0fc0bbe85331a03208041f1d681
Oleh Kuzmenko <oleg24453@gmail.com>
2020-08-27 21:58:38 +0300

    added model CurrentRate, CurrentRateDto and CurrentRateMapper

---
+++ src/main/java/com/current/exchange/exchange/model/CurrentRate.java
@@ -0,0 +1,14 @@
+package com.current.exchange.exchange.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class CurrentRate {
+    private String name;
+    private BigDecimal rate;
+    private LocalDate date;
+}
---
+++ src/main/java/com/current/exchange/exchange/model/mapper/CurrentRateMapper.java
@@ -0,0 +1,20 @@
+package com.current.exchange.exchange.model.mapper;
+
+import com.current.exchange.exchange.model.CurrentRate;
+import com.current.exchange.exchange.model.dto.CurrentRateDto;
+import org.springframework.stereotype.Component;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class CurrentRateMapper {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public CurrentRate getCurrencyFromDto(CurrentRateDto currentRateDto) {
+        return new CurrentRate(
+                currentRateDto.getTxt(),
+                BigDecimal.valueOf(Double.parseDouble(currentRateDto.getRate())),
+                LocalDate.parse(currentRateDto.getExchangedate(), FORMATTER));
+    }
+}
